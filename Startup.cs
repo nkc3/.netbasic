5542d7ef730c09e072240115ec43354ccb05c5bd
Naveen K Chinnireddy <nkr@hotmail.com>
2018-07-19 17:10:34 +0530

    adding to gitrepo

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jenkins_msbuild.Startup))]
+namespace Jenkins_msbuild
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
